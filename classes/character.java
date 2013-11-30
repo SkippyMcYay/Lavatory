9f3eaa4614a9a694b3b26ff3954bb30fb3d66b11
Feraldragon <drake681743@hotmail.com>
2013-11-29 19:27:12 -0800

    Created the character class and added a few variables

---
+++ classes/character.java
@@ -0,0 +1,10 @@
+
+public class character {
+
+    private int max_hp,hp=100,100
+    private int max_mp,mp=100,100
+    private int attack,attack_range=0,1
+    private int defense=0
+    private int turn_ap_cost,max_ap,ap=60,100,0
+    
+}
