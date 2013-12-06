2c0d27a5dc426ff1709a1ee163e518154f2f6300
Feraldragon <drake681743@hotmail.com>
2013-12-06 00:01:27 -0800

    Added the  board class and then poured functions that probably don't belong with it into it. with enjoyable results.

--- game/test.java
+++ game/test.java
@@ -1,12 +1,17 @@
-// to access a file the string should read something like ".\\game\\display\\file.java"
-import entity.character;
+// to access a file the string should read something like ".\\game\\folder\\file.java"
+import entity.*;
+
 public class test{
     public static void main(String args[]){
-        character james=new character();
-        int new_position[] = {2,0};
-        james.setPos(new_position);
-        System.out.println(james.getPos()[0]);
-        System.out.println(james.getPos()[1]);
+        character person=new character("james");
+        board world=new board();
 
+        world.displayBoard();
+        world.addCharacter(person);
+        world.displayBoard();
+
+        character someone=new character("Indestructible James");
+
+        world.removeCharacter(someone);
     }
 }
