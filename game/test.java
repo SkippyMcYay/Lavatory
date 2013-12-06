8f745aa84fdee91162b16cf5ad17b38a8ca2e067
Feraldragon <drake681743@hotmail.com>
2013-12-05 19:25:56 -0800

    Bugtested and improved the character class

--- game/test.java
+++ game/test.java
@@ -1,11 +1,12 @@
+// to access a file the string should read something like ".\\game\\display\\file.java"
 import entity.character;
-import display.james_frame_test;
-
 public class test{
     public static void main(String args[]){
-        james_frame_test james=new james_frame_test();
-        String image=".\\game\\display\\Images\\hampsterpunch.bmp";
-        String title="Their world ends now!";
-        james.CreateWindow(image,title);
+        character james=new character();
+        int new_position[] = {2,0};
+        james.setPos(new_position);
+        System.out.println(james.getPos()[0]);
+        System.out.println(james.getPos()[1]);
+
     }
 }
